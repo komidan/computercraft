625b3c64f8e12361d88f56c5c711136a110ac7ef
komidan <komidan.gg@gmail.com>
2025-04-02 14:24:57 -0400

    smol changes

--- install.lua
+++ install.lua
@@ -15,7 +15,7 @@ print("ChocolatOS Install/Update")
 term.setTextColor(colors.yellow)
 print("WARNING: It is suggested to kill any scripts before installing/updating!")
 term.setTextColor(colors.white)
-print("\nPress any button to continue.")
+print("\nPress any button to continue...")
 read()
 
 local onDisk = nil
@@ -28,25 +28,31 @@ if isDiskPresent then
 	elseif input == "n" then
 		onDisk = false
 	else
-		print("Invalid Input: '" .. input .. "' try again.")
+		print("Invalid Input '" .. input .. "' try again.")
 		goto diskInstall
 	end
 end
 
 -- remove old files
+print("removing old files...")
 fs.delete("disk/chocolat/")
 fs.delete("chocolat/")
 
 -- get all the new files from github
+print("retrieving files from github...")
 for i = 1, #URLS do
 	shell.run("wget " .. URLS[i])
 end
 
 -- move all files to correct paths
+print("moving files from /root/ to /chocolat/")
 fs.move("klib.lua", "chocolat/lib/klib.lua")
 fs.move("chocolat.lua", "chocolat/os/chocolat.lua")
 fs.copy("startup.lua", "chocolat/os/startup_copy.lua") -- incase you want to transfer the OS between systems on the disk itself
 fs.move("install.lua", "chocolat/install.lua")
-if onDisk then fs.move("chocolat", "disk/chocolat") end
+if onDisk then
+	print("copying over to /disk/")
+	fs.move("chocolat", "disk/chocolat")
+end
 
 os.reboot()
